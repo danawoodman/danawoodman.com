eaf75e59b9d2a570d408c96b9b992256ab7e0054
Dana Woodman <dana@danawoodman.com>
2022-01-30 21:51:38 -0800

    Add some nav links

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <header class="mb-12 text-center">
+  <h1>SvelteHelp</h1>
   <nav>
     <a href="/">Home</a>
+    <a href="/">How Tos</a>
+    <a href="/">References</a>
+    <a href="/">Resources</a>
   </nav>
 </header>
 
