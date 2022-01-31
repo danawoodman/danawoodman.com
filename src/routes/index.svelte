149bd1b4729a34c41914de5c8c2ea940d19efce9
Dana Woodman <dana@danawoodman.com>
2022-01-30 21:51:25 -0800

    Create generic CategoryBreadcrumbs component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,8 @@
 </script>
 
 <script lang="ts">
+  import CategoryBreadcrumbs from "$lib/CategoryBreadcrumbs.svelte";
+
   export let posts = [];
 </script>
 
@@ -16,9 +18,14 @@
   <h1 class="text-3xl mb-6 font-bold">Posts</h1>
   <nav>
     {#each posts as post}
-      <article class="my-4">
-        <h2 class="text-xl">
-          <a href={post.slug} class="underline">{post.title}</a>
+      <article class="my-12">
+        <div class="mb-4">
+          <CategoryBreadcrumbs categories={post.categories} />
+        </div>
+        <h2
+          class="font-heading text-3xl font-extrabold hover:text-brand transition-colors"
+        >
+          <a href={post.slug} class="">{post.title}</a>
         </h2>
         {#if post.description}
           <div class="prose mt-1">{post.description}</div>
