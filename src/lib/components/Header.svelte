2d6d999062dc3f5a1895fc4266a41c2f249c76c3
Dana Woodman <dana@danawoodman.com>
2022-07-08 14:56:28 -0700

    Update header/footer/nav components

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,22 +1,35 @@
 <script>
 	import Avatar from './Avatar.svelte'
 	import Nav from './Nav.svelte'
+	import SocialLinks from './SocialLinks.svelte'
 </script>
 
-<div class="mb-12" id="page-header">
-	<div class="flex items-center max-w-prose mx-auto py-6">
-		<header>
-			<div class="flex-shrink-0 flex items-center">
-				<a href="/">
-					<Avatar size={40} />
-				</a>
-				<a href="/">
-					<h1 class="ml-4 font-bold">Dana Woodman</h1>
-				</a>
-			</div>
-		</header>
-		<div class="ml-auto">
+<div id="page-header" class="md:flex md:items-center max-w-4xl mx-auto py-4">
+	<header>
+		<div class="flex-shrink-0 flex items-center">
+			<a href="/">
+				<Avatar size={40} />
+			</a>
+			<a href="/">
+				<h1 class="ml-4 font-bold">
+					<span class="enlarge">D</span>ANA
+					<span class="enlarge">W</span>OODMAN
+				</h1>
+			</a>
+		</div>
+	</header>
+	<div class="md:ml-auto md:flex flex-col md:flex-row md:items-center">
+		<div class="mt-4 md:mt-0">
 			<Nav />
+		</div>
+		<div class="md:ml-10 mt-4 md:mt-0">
+			<SocialLinks />
 		</div>
 	</div>
 </div>
+
+<style lang="postcss">
+	.enlarge {
+		font-size: 1.2em;
+	}
+</style>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,9 @@
-<nav class="flex items-center">
+<nav class="flex items-center text-lg">
 	<!-- <a href="/about">About</a> -->
-	<a href="/writing" class="px-4 py-2">Writing</a>
-	<a href="/resume" class="px-4 py-2">Resume</a>
-	<!-- <a href="/tools" class="px-4 py-2">Tools</a> -->
+	<a href="/" class="mr-4 py-2 hover:text-emerald-600">Home</a>
+	<a href="/writing" class="mr-4 py-2 hover:text-emerald-600">Writing</a>
+	<a href="/resume" target="_blank" class="py-2 hover:text-emerald-600"
+		>Resume</a
+	>
+	<!-- <a href="/projects" class="px-4 py-2">Projects</a> -->
 </nav>
