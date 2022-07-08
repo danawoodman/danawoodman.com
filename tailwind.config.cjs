7eb22df05e78bb94cd429cdefa9027e8e1672b39
Dana Woodman <dana@danawoodman.com>
2022-07-08 14:54:20 -0700

    Change to tabs, add vite config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,36 @@
-const theme = require("tailwindcss/defaultTheme");
+const theme = require('tailwindcss/defaultTheme')
 
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    fontFamily: {
-      heading: "Montserrat, sans-serif",
-      body: "'Open Sans', sans-serif",
-    },
-    extend: {
-      colors: {
-        brand: "#ff3e00",
-      },
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            "code::before": { content: '""' },
-            code: {
-              background: theme("colors.gray.100"),
-              border: `1px solid ${theme("colors.gray.200")}`,
-              borderRadius: theme("borderRadius.sm"),
-              padding: `${theme("spacing.1")} ${theme("spacing.2")}`,
-            },
-            "code::after": { content: '""' },
-          },
-        },
-      }),
-    },
-  },
+	theme: {
+		fontFamily: {
+			heading: 'Montserrat, sans-serif',
+			body: "'Open Sans', sans-serif",
+			// mono: 'monospace',
+		},
+		extend: {
+			colors: {
+				brand: '#ff3e00',
+			},
+			typography: ({ theme }) => ({
+				DEFAULT: {
+					css: {
+						'code::before': { content: '""' },
+						code: {
+							background: theme('colors.gray.100'),
+							border: `1px solid ${theme('colors.gray.200')}`,
+							borderRadius: theme('borderRadius.sm'),
+							padding: `${theme('spacing.1')} ${theme('spacing.2')}`,
+						},
+						'code::after': { content: '""' },
+					},
+				},
+			}),
+		},
+	},
 
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+}
 
-module.exports = config;
+module.exports = config
